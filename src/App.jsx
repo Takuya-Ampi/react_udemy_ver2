4c8afe02feb2e14461f370932a2ca0810170ddda
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-06-30 07:03:17 +0900

    &&を使ったコンポーネントの表示切り替え

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickToggle = () => setOpen(!open);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input
+        placeholder="入力してください"
+        value={text}
+        onChange={onChangeText}
+      />
+      <button onClick={onClickToggle}>表示</button>
+      {open && <ChildArea />}
     </div>
   );
 }
