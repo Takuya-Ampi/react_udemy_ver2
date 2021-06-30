fbd2f5371cbb3f1848d0c495f30ddeb357d6a04a
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-06-30 09:37:34 +0900

    memo化

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,10 +1,13 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "pink"
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
+  console.log("レンダリングスタート");
   const { open } = props;
   return (
     <>
@@ -15,4 +18,4 @@ export const ChildArea = (props) => {
       )}
     </>
   );
-};
+});
