24bae4a36377cd71f0f5abefb9386a1f167f53cd
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-07-02 09:55:23 +0900

    ルーティング設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React, { useState, useCallback } from "react";
-import { ChildArea } from "./ChildArea";
+// import React, { useState, useCallback } from "react";
+// import { ChildArea } from "./ChildArea";
+
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-  const onChangeText = (e) => setText(e.target.value);
-  const onClickToggle = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), []);
+  // const [text, setText] = useState("");
+  // const [open, setOpen] = useState(false);
+  // const onChangeText = (e) => setText(e.target.value);
+  // const onClickToggle = () => setOpen(!open);
+  // const onClickClose = useCallback(() => setOpen(false), []);
 
   return (
     <>
-      <div className="App">
+      {/* <div className="App">
         <input
           placeholder="入力してください"
           value={text}
@@ -19,7 +24,28 @@ export default function App() {
         />
         <button onClick={onClickToggle}>{open && "非"}表示</button>
         <ChildArea open={open} onClickClose={onClickClose} />
-      </div>
+      </div> */}
+
+      <BrowserRouter>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/page1">Page1</Link>
+          <br />
+          <Link to="/page2">Page2</Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/page1">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
