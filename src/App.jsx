2b5cb99440a827b6969c400ffba113094d52acc9
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-07-05 09:49:04 +0900

    ネストされたルーティングの実装

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -38,7 +40,22 @@ export default function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/page1" render={() => <Page1 />} />
+          <Route
+            path="/page1"
+            render={() => (
+              <Switch>
+                <Route exact path="/page1">
+                  <Page1 />
+                </Route>
+                <Route path="/page1/detailA">
+                  <Page1DetailA />
+                </Route>
+                <Route path="/page1/detailB">
+                  <Page1DetailB />
+                </Route>
+              </Switch>
+            )}
+          />
           <Route path="/page2">
             <Page2 />
           </Route>
