ba0ef6494cd214da0fe02e8f958a9ca0ac2e17f8
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-07-18 22:59:02 +0900

    ルーティング

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 // import React, { useState, useCallback } from "react";
 // import { ChildArea } from "./ChildArea";
 
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./page1DetailB";
-import { Page2 } from "./Page2";
+import { BrowserRouter, Link } from "react-router-dom";
+
 import "./styles.css";
+
+import { Router } from "./router/Router";
 
 export default function App() {
   // const [text, setText] = useState("");
@@ -36,32 +34,7 @@ export default function App() {
           <br />
           <Link to="/page2">Page2</Link>
         </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route
-            path="/page1"
-            render={(
-              { match: { url } } // propsの中にmatchオブジェクトがある
-            ) => (
-              <Switch>
-                <Route exact path={url}>
-                  <Page1 />
-                </Route>
-                <Route path={`${url}/detailA`}>
-                  <Page1DetailA />
-                </Route>
-                <Route path={`${url}/detailB`}>
-                  <Page1DetailB />
-                </Route>
-              </Switch>
-            )}
-          />
-          <Route path="/page2">
-            <Page2 />
-          </Route>
-        </Switch>
+        <Router />
       </BrowserRouter>
     </>
   );
